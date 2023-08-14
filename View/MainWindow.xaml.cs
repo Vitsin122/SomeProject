d94c67dd7205d8aba42dd404c72b6e9258479e85
Vitsin <warfire122@gmail.com>
2023-08-14 20:29:37 +0800

    A converter for coefficients A and B is made, the converter is added to binding

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,19 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Xceed.Document.NET;
-using Xceed.Words.NET;
+﻿using System.Windows;
+using SomeProject.View.Converters;
 
 namespace SomeProject.View
 {
@@ -26,7 +12,7 @@ namespace SomeProject.View
         {
             InitializeComponent();
 
-            
+            DataContext = new SomeProject.ViewModel.ViewModelProvider();
         }
     }
 }
