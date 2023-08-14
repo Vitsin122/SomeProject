e76402f5f2be2417f4c258bcd01bd13903e0426c
Vitsin <warfire122@gmail.com>
2023-08-14 20:29:37 +0800

    A converter for coefficients A and B is made, the converter is added to binding

---
+++ View/Converters/StrToIntConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace SomeProject.View.Converters
+{
+    public class StrToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int)
+                return (int)value;
+            else
+                return " ";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (int.TryParse(value.ToString(), out int result))
+                return result;
+            else
+                return 0;
+        }
+    }
+}
--- ViewModel/ViewModelProvider.cs
+++ ViewModel/ViewModelProvider.cs
@@ -5,15 +5,18 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using SomeProject.View;
+using SomeProject.Model;
 
 namespace SomeProject.ViewModel
 {
     public class ViewModelProvider : INotifyPropertyChanged
     {
-        
-
-
+        public TextBlock? SelectedRB { get; set; } = null;
+        public int A { get; set; } = 0;
+        public int B { get; set; } = 0;
 
 
 
